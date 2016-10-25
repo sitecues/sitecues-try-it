03f761e4159c6d641d607bdcb4a07ae5e7051c68
Seth Holladay <me@seth-holladay.com>
2016-10-24 22:19:06 -0400

    init

---
+++ README.md
@@ -0,0 +1,45 @@
+# sitecues-try-it [![Build status for sitecues-try-it on Circle CI.](https://img.shields.io/circleci/project/sitecues/sitecues-try-it/master.svg "Circle Build Status")](https://circleci.com/gh/sitecues/sitecues-try-it "Sitecues Try It Builds")
+
+> Simulate Sitecues on your website.
+
+## Why?
+
+ - Some.
+ - Various.
+ - Reasons.
+
+## Install
+
+```sh
+npm install sitecues-try-it --save
+```
+
+## Usage
+
+Get it into your program.
+
+```js
+const sitecuesTryIt = require('sitecues-try-it');
+```
+
+## API
+
+### sitecuesTryIt()
+
+It throws an error and is not ready yet.
+
+## Contributing
+
+See our [contributing guidelines](https://github.com/sitecues/sitecues-try-it/blob/master/CONTRIBUTING.md "The guidelines for participating in this project.") for more details.
+
+1. [Fork it](https://github.com/sitecues/sitecues-try-it/fork).
+2. Make a feature branch: `git checkout -b my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin my-new-feature`
+5. [Submit a pull request](https://github.com/sitecues/sitecues-try-it/compare "Submit code to this project for review.").
+
+## License
+
+[MPL-2.0](https://github.com/sitecues/sitecues-try-it/blob/master/LICENSE "The license for sitecues-try-it.") © [Seth Holladay](http://seth-holladay.com "Author of sitecues-try-it.")
+
+Go make something, dang it.
---
+++ index.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const { Server } = require('hapi');
+const hi = require('hapi-hi');
+const h2o2 = require('h2o2');
+/* eslint-disable global-require */
+const routes = [
+    require('./route/target'),
+    require('./route/stream-target')
+];
+/* eslint-enable global-require */
+
+class ReverseProxy extends Server {
+
+    constructor(option) {
+        const config = Object.assign(
+            {
+                port : 8001,
+                tls  : {
+                    // Async configuration for constructors is a pain.
+                    /* eslint-disable no-sync */
+                    key  : fs.readFileSync(path.join(__dirname, 'ssl/key/localhost.key')),
+                    cert : fs.readFileSync(path.join(__dirname, 'ssl/cert/localhost.cert'))
+                    /* eslint-enable no-sync */
+                }
+            },
+            option
+        );
+
+        super();
+        super.connection({
+            port : config.port,
+            tls  : config.tls
+        });
+    }
+
+    start() {
+        const plugins = [
+            {
+                register : hi,
+                options  : {
+                    cwd        : __dirname,
+                    noConflict : true
+                }
+            },
+            h2o2
+        ];
+        return super.register(plugins).then(() => {
+            super.route(routes);
+
+            // Sadly, we cannot just return the start() promise because of:
+            // https://github.com/hapijs/hapi/issues/3217
+
+            return new Promise((resolve, reject) => {
+                super.start((err) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+
+                    resolve();
+                });
+            });
+        });
+    }
+}
+
+module.exports = {
+    ReverseProxy
+};
